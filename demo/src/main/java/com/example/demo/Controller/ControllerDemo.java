ea2b23af1f85577349932707ef57f6a5908e9640
mai.it03 <maidx26092003@gmail.com>
2026-07-01 16:50:29 +0700

    feature: fix controller demo

--- demo/src/main/java/com/example/demo/Controller/ControllerDemo.java
+++ demo/src/main/java/com/example/demo/Controller/ControllerDemo.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class ControllerDemo {
     @GetMapping("/")
     public String hello() {
-        return "Hello World";
+        return "Hello World 123";
     }
 }
